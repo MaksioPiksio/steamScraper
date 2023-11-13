0c942462a42c1fe215e1a9b61b40026058af30da
Koniczyn <koniczynszef@gmail.com>
2023-11-13 13:03:48 +0100

    change to id

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const app = express();
 
 app.use(cors());
 
-app.use("/hello", (req, res) => {
-    res.send("Hello World!");
-})
+// app.use("/hello", (req, res) => {
+//     res.send("Hello World!");
+// })
 
-// app.get("/:goods_id", (req, res) => {
-//     const { goods_id } = req.params;
-//     const url = `https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=${goods_id}`;
-//     req.pipe(request(url)).pipe(res);
-// });
+app.get("/:goods_id", (req, res) => {
+    const { goods_id } = req.params;
+    const url = `https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=${goods_id}`;
+    req.pipe(request(url)).pipe(res);
+});
 
 app.listen(8459, () => {
     console.log(`Port 8459`);
