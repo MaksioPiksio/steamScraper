3627b9fa863f954334404764580b308fdeb34ba1
MaksioPiksio <maksio.piksio@icloud.com>
2023-11-18 23:08:12 +0100

    Bugs | fixing show

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import request from "request";
 import { getinventory } from "get-steam-inventory";
 
 var steamid = "76561198999425153";
-let t = "s";
+let t = "";
 const bulkList = (steamid) => {
     getinventory(730, steamid, 2)
         .then((data) => {
             t = data.marketnames;
+            t = test;
         })
         .catch((err) => {
             t = "(" + err + " user - " + steamid + ")";
